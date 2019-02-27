45546745d379352d416f0c8f34703864e0482455
joepak <josephpak2014@gmail.com>
2019-02-26 19:05:16 -0500

    Initial MVP for one tab

--- apple-nav/src/components/Nav.js
+++ apple-nav/src/components/Nav.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Route } from 'react-router-dom';
+
+// Top Nav Images
+import apple from './apple.png';
+import search from './search.png';
+import shop from './shop.png';
 
 import TopNav from './TopNav';
+import SubNav from './SubNav';
 
 const NavWrapper = styled.div`
-    height: 48px;
     background-color: rgba(45,45,45,0.98);
     display: flex;
     z-index: 9999;
-    position: absolute;
     width: 100%;
+    flex-direction: column;
 `
 
-const NavContent = styled.div`
+const TopNavContent = styled.div`
     margin: 0 auto;
+    height: 44px;
     max-width: 980px;
     padding: 0 22px;
-    position: relative;
-    z-index: 2;
     display: flex;
     justify-content: space-between;
     width: 100%;
@@ -44,18 +49,61 @@ const NavContent = styled.div`
     }
 `
 
-const Nav = props => {
-    return (
-        <NavWrapper>
-            <NavContent>
-                {props.navdata.map(tab => (
-                    <TopNav 
-                    tab={tab}
-                    />
-                ))}
-            </NavContent>
-        </NavWrapper>
-    )
+const SubNavContent = styled.div`
+    height: 92px;
+    max-width: 980px;
+    padding: 0 186px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    background-color: rgba(45,45,45,0.98);;
+
+    img {
+        opacity: 1;
+    }
+
+    a {
+        text-decoration: none;
+        color: rgb(255, 255, 255);
+        font-size: 11px;
+
+        &:hover {
+            opacity: .65;
+        }
+    }
+`
+
+class Nav extends React.Component {
+    constructor() {
+        super()
+    }
+    
+    render() {
+        return (
+            <NavWrapper>
+                <TopNavContent>
+                    <a><img className="apple-logo" src={apple}/></a>
+                    {this.props.navdata.map(tab => (
+                        <TopNav 
+                        key={tab.path}
+                        tab={tab}
+                        changeSelected={this.changeSelected}
+                        />
+                    ))}
+                    <a>Support</a>
+                    <a><img className="search-logo" src={search}/></a>
+                    <a><img className="shop-logo" src={shop}/></a>
+                </TopNavContent>
+                <SubNavContent>
+                    <Route path="/mac" render={props => (
+                        <SubNav {...props} navdata={this.props.navdata} />
+                    )}></Route>
+                </SubNavContent>
+            </NavWrapper>
+        )
+    }
+    
 }
 
 export default Nav;
--- apple-nav/src/components/TopNav.js
+++ apple-nav/src/components/TopNav.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
+import SubNav from './SubNav';
 import styled from 'styled-components';
-import navdata from '../nav-data';
 
-const TopNav = props => {
-    return (
-      <NavLink to={props.tab.path}>{props.tab.content}</NavLink>  
-    );
+class TopNav extends React.Component {
+
+    render() {
+        return (
+            <>
+            <NavLink 
+            to={this.props.tab.path}
+            >{this.props.tab.content}</NavLink>  
+            </>  
+        );
+    } 
 }
 
 export default TopNav;
